5f8d2c4e9dc09d6636b184ea9bee44d2e1160bdc
victorhugoaguilar <victorhugoaguilar@aol.com>
2022-06-25 00:14:34 +0200

    fix test

--- test/functions.test.js
+++ test/functions.test.js
@@ -2,7 +2,7 @@ const ormUser = require('../domain/orm/orm-user');
 
 describe('Post Endpoints', () => {
     it('should info of a USER', async () => {
-        const res = await ormUser.GetById("76b446ec-e739-496c-a639-8c42c9cf0f00");
-        expect(res.name).toEqual("joanna");
+        const res = await ormUser.GetById("35a3f13d-7e94-4df6-8996-ccb1f4a77295");
+        expect(res.name).toEqual("Javier Eduardo");
     });
 });
